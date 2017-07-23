71dae0fdbd989cbc8265a48ec754b84f27fd6978
Carlos <cjx3m@hotmail.com>
2017-07-23 15:38:12 -0500

    Initial project commit
    Idle Animation 1st try

---
+++ Assets/Scripts/AnyaMove.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnyaMove : MonoBehaviour {
+
+	public Animator anim;
+	private float walkSpeed;
+	private float fallSpeed;
+	private float verticalMovement;
+	private float horizontalMovement;
+	private bool onGround;
+	private bool isIdle;
+	private bool isWalkingFwd;
+	private bool isWalkingBck;
+	private WalkDirection walkDirection;
+	// Use this for initialization
+	void Start () {
+		onGround = true;
+		isIdle = true;
+		walkDirection = WalkDirection.Right;
+		walkSpeed = 0.05f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.RightArrow) && onGround) {
+			isIdle = false;
+			isWalkingFwd = true;
+			horizontalMovement = walkSpeed;			
+		}
+		if (Input.GetKeyUp(KeyCode.RightArrow) && onGround) {
+			isWalkingFwd = false;
+			isIdle = true;
+		}
+
+	}
+
+	private enum WalkDirection
+	{
+		Right = 0,
+		Left = 1
+	}
+}
